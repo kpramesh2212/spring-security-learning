53595061a57838e6b2a7666728a3a816daa7ff55
Ramesh Kurichi Ponnuswamy <rameshkp@ie.ibm.com>
2017-03-24 17:12:42 +0000

    Hello World Servlet no Auth

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/hello"})
+public class HelloWorldServlet extends HttpServlet {
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.getWriter().write("Hello World");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
